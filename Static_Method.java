d61b06539d40ad2bf7a8a38e193fc1b9a41e2f31
MMeghana <63573802+MMeghana@users.noreply.github.com>
2020-08-31 14:48:33 +0530

    Add files via upload

---
+++ Static_Method.java
@@ -0,0 +1,17 @@
+package Pack1;
+class Test1
+{
+	static int x =10;
+	static void show()
+	{
+		System.out.println(" static value = "+ x);
+	}
+}
+public class Static_Method 
+{
+	public static void main(String[] args) 
+	{
+	  Test1.show();
+	}
+
+}
